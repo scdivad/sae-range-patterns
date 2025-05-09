54843cc9b09084fda1ad965baa6ad5a8253ebb25
David Cheung <davidsc2@illinois.edu>
2025-05-09 08:04:22 +0000

    comments on parallel

--- format_sfc/format.py
+++ format_sfc/format.py
@@ -1,8 +1,10 @@
 import json
 
-formatted_data = []
 incl_patch = True
-file_path = "../data/sva/rc_train.json"
+file_path = "../data/codereason/key/data_len5_digit1.json"
+output_file_path = "formatted_sva.json"
+# output_file_path = "formatted_key.json"
+# output_file_path = "formatted_key_no_traceback.json"
 
 def get_label_sva(prefix, answer):
     # if answer in [' go',  ' are', ' do',  ' have']:
@@ -28,17 +30,21 @@ def format_example(prefix, answer, get_label):
         'label': label,
     }
 
-# Specify the output file path
-output_file_path = "formatted_rc_train.json"
-
 with open(file_path, "r") as f, open(output_file_path, "w") as out_f:
     get_label = get_label_sva
     for line in f:
         record = json.loads(line)
         if (tmp := format_example(record['clean_prefix'], record['clean_answer'], get_label)):
-            formatted_data.append(tmp)
+            out_f.write(json.dumps(tmp) + "\n")
         if incl_patch and (tmp := format_example(record['patch_prefix'], record['patch_answer'], get_label)):
-            formatted_data.append(tmp)
-        
-        # Write the formatted data to the output file as JSON
-        out_f.write(json.dumps(tmp) + "\n")
+            out_f.write(json.dumps(tmp) + "\n")
+# with open(file_path, "r") as f, open(output_file_path, "w") as out_f:
+#     get_label = get_label_is_answer
+#     tmp = json.load(f)
+#     for record in tmp:
+#         if (tmp := format_example(record['clean_prefix'], record['clean_answer'], get_label)):
+#             if tmp['label'] != "Traceback":
+#                 out_f.write(json.dumps(tmp) + "\n")
+#         if incl_patch and (tmp := format_example(record['patch_prefix'], record['patch_answer'], get_label)):
+#             if tmp['label'] != "Traceback":
+#                 out_f.write(json.dumps(tmp) + "\n")
